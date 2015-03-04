9f603ba35bfcb1757520ddc02e9f85ca86f025fc
Noah Peart <noah.v.peart@gmail.com>
2015-03-03 21:23:32 -0500

    Batch update

--- vars/heights/canopy.R
+++ vars/heights/canopy.R
@@ -3,45 +3,48 @@
 ## Description: Estimate canopy height/dbh
 ## Author: Noah Peart
 ## Created: Mon Mar  2 14:31:12 2015 (-0500)
-## Last-Updated: Mon Mar  2 15:00:05 2015 (-0500)
+## Last-Updated: Tue Mar  3 18:58:31 2015 (-0500)
 ##           By: Noah Peart
 ######################################################################
-library(plyr)
 source("~/work/ecodatascripts/read/read-moose.R")
-yrs <- c(86, 87, 98, 10) ## yrs to calculate canopy heights
 
-canhts <- ddply(pp, .(PPLOT), .fun = function(x) {
-    x <- droplevels(x)
-    nCodoms <- sapply(yrs, function(y) {
-        yr <- y
-        if (unique(x$PPLOT) == 16 && yr == 87 ||
-            unique(x$PPLOT) == 22 && yr == 87) { yr <- 98 } ## use trees from 98 for plots 16&22
-        stat <- x[, paste0("STAT",yr)]
-        if(unique(x$PPLOT) == 15 && yr == 86) yr <- 87 ## use heights from 87
-        htCol <- x[, paste0("HTTCR",yr)]
-        pp <- x[!is.na(htCol) & stat == "ALIVE",]
-        ifelse (y == 86, yy <- 88, yy <- yr) ## use can heights from 88 for 86 plots
-        cpos <- paste0("CPOS", yy)
-        codHeights <- pp[pp[, cpos] == "c", paste0("HTTCR",yr)]
-        mean(codHeights) ## calculate canopy heights
+## Returns estimated canopy height and dbh of canopy trees
+## (simply the average of trees with certain crown position)
+## NOTE: 1. By default uses crown positions from 88 for 86 trees, ("cposyrs" variable)
+##       2. For plot 15, heights were measured in 87 (modify "htyrs" for this)
+canopy_dims <- function(dat, plot=NULL, yrs=c(86, 87, 98, 10), htyrs = yrs, cposyrs=c(88, 87, 98, 10),
+                        cols = list(plot="PPLOT", stat="STAT", ht="HTTCR", dbh="DBH", cpos="CPOS"),
+                        canpos = c("c", "d")) {
+    if (!missing(plot)) dat <- dat[dat[[cols$plot]] == plot, ]
+    if (unique(dat[[cols$plot]]) == 15) htyrs[1] <- 87  # plot 15 hts measured in 87 for 86 dbhs
+    res <- sapply(1:length(yrs), FUN = function(i) {
+        inds <- (dat[[paste0(cols$stat, yrs[i])]] == "ALIVE") &
+            (!is.na(dat[[paste0(cols$ht, htyrs[i])]])) &
+                (dat[[paste0(cols$cpos, cposyrs[i])]] %in% canpos)
+        c(ht = mean(dat[inds,][[paste0(cols$ht, htyrs[i])]]),   # canopy height
+          dbh = mean(dat[inds,][[paste0(cols$dbh, yrs[i])]]))   # canopy dbh
     })
-})
-names(canhts) <- c("PPLOT", paste0("CANHT",yrs))
+    data.frame(t(res), time = yrs)
+}
 
-## Canopy dbhs
-candbhs <- ddply(pp, .(PPLOT), .fun = function(x) {
-    x <- droplevels(x)
-    canDBH <- sapply(yrs, function(y) {
-        yr <- y
-        canHt <- canhts[canhts$PPLOT==unique(x$PPLOT),paste0("CANHT",yr)]
-        stat <- x[, paste0("STAT",yr)]
-        ifelse (unique(x$PPLOT) == 15 & yr == 86, ## use heights from 87 for plot 15
-            { htCol <- x[, paste0("HTTCR",87)]; colName <- paste0("HTTCR",87) },
-               { htCol <- x[, paste0("HTTCR", yr)]; colName <- paste0("HTTCR",yr) })
-        pp <- x[!is.na(htCol) & stat == "ALIVE",]
-        dbhs <- pp[which(pp[, colName] > canHt), paste0("DBH", yr)]
-        mean(dbhs, na.rm = TRUE) ## calculate canopy heights
-    })
-})
-names(candbhs) <- c("PPLOT", paste0("CANDBH",yrs))
+## All plots canopy heights
+library(dplyr)
+library(magrittr)
+can_dims <- pp %>% group_by(PPLOT) %>% do(canopy_dims(., canpos=c("c","d"))) %>% filter(PPLOT > 3)
 
+## Compare to previous estimates
+## prev <- distinct(pp, cht8687, cht98, CHT10) %>% select(PPLOT, cht8687, cht98, CHT10) %>%
+##     filter(complete.cases(.)) %>% print
+## nw <- reshape(data.frame(can_dims), direction="wide", timevar="time", idvar="PPLOT") %>%
+##     mutate(ht8687 = na.omit(c(ht.86,ht.87))) %>% select(PPLOT, ht8687, ht.98, ht.10) %>% print
+
+## Visualize
+## with(can_dims, stripchart(time~ht, col = PPLOT, jitter=1))
+## p4 <- pp %>% filter(PPLOT == 4, STAT86 == "ALIVE") %>% select(ends_with("86"))
+## p4can <- can_dims %>% filter(PPLOT == 4, time == 86)
+## with(p4, plot(DBH86, HTTCR86))
+
+## p9 <- pp %>% filter(PPLOT == 9, STAT86 == "ALIVE") %>% select(ends_with("86"))
+## p9can <- can_dims %>% filter(PPLOT == 9, time == 86)
+## with(p9, plot(DBH86, HTTCR86))
+## points(p9can$dbh, p9can$ht, pch = 16, col = "darkred")
--- vars/heights/predict_heights.R
+++ vars/heights/predict_heights.R
@@ -3,10 +3,50 @@
 ## Description: Predict missing heights for trees from basal areas for Moosilauke
 ## Author: Noah Peart
 ## Created: Mon Mar  2 13:37:06 2015 (-0500)
-## Last-Updated: Mon Mar  2 14:29:10 2015 (-0500)
+## Last-Updated: Tue Mar  3 21:20:42 2015 (-0500)
 ##           By: Noah Peart
 ######################################################################
+source("~/work/ecodatascripts/vars/heights/canopy.R")
+## dat <- read.csv("~/work/data/moose/moose-wide.csv")
+
 library(splines)
 library(dplyr)
-dat <- read.csv("~/work/data/moose/moose-wide.csv")
+
+## Visualize
+## with(can_dims, stripchart(time~ht, col = PPLOT, jitter=1))
+plot <- c(13:15, 25:27)
+p4 <- pp %>% filter(PPLOT %in% plot, STAT10 == "ALIVE") %>% select(PPLOT, SPLOT, ASPCL, ends_with("10"))
+p4can <- can_dims %>% filter(PPLOT %in% plot, time == 10)
+
+p4$ind <- ifelse(p4$ASPCL == "E", 1, 2)
+
+with(p4, plot(DBH10, HTTCR10, pch = 15, col = ind))
+points(p4can$dbh, p4can$ht, col = "blue", pch = 16)
+
+p4 %>% filter(!is.na(HTTCR10)) %>% distinct(SPLOT) %>% print
+    
+## Models
+basis <- bs(p4$DBH86, knots=c(p4can$dbh))
+summary(fit <- lm(HTTCR86 ~ bs(DBH86, knots=c(p4can$dbh)), data = p4))
+summary(fit1 <- lm(HTTCR86 ~ DBH86, data = p4))
+summary(fit2 <- nls(HTTCR86 ~ a*DBH86**b, data = p4, start = list(a=0.1, b=1)))
+summary(fit3 <- lm(HTTCR86 ~ bs(DBH86, knots = c(70), degree = 2), data = p4))
+## with(pp, hts <- c(DBH86, DBH87, )
+summary(fit4 <- lm(HTTCR86 ~ bs(DBH86) + ELEVCL + ASPCL, data = pp))
+## Below/above canopy
+
+
+## Predict
+dbhs <- seq(5, 45, length=100)
+pred1 <- predict(fit, newdata = data.frame(DBH86=dbhs))
+lines(dbhs, pred1, col = "blue", lwd =3)
+
+dbhs <- seq(5, 45, length=100)
+pred2 <- predict(fit3, newdata = data.frame(DBH86=dbhs))
+lines(dbhs, pred2, col = "green", lwd =3)
+
+abline(fit1, col = "green")
+
+pred2 <- predict(fit2, newdata = data.frame(DBH86 = dbhs))
+points(dbhs, pred2, col = "purple", type="l")
 
