43e1efc5237b45d88362eb84fda6ae065597c992
Noah Peart <noah.v.peart@gmail.com>
2015-04-03 11:38:53 -0400

    run

--- vars/boles/run.R
+++ vars/boles/run.R
@@ -4,7 +4,7 @@
 ## Description: Wrapper to run height/bole calculations
 ## Author: Noah Peart
 ## Created: Thu Apr  2 22:43:07 2015 (-0400)
-## Last-Updated: Thu Apr  2 23:44:58 2015 (-0400)
+## Last-Updated: Fri Apr  3 11:36:49 2015 (-0400)
 ##           By: Noah Peart
 ######################################################################
 cat("\n\n\t\tChecking/installing packages...\n\n")
@@ -13,10 +13,12 @@ for (pack in packages)
     if (pack %in% rownames(installed.packages()) == FALSE)
         install.packages(pack)
 
-if (!("home" %in% list.files("C:\\")) | !("work" %in% list.files("C:\\home"))) {
-    cat("Need to create 'C:\\home\\work' directory and move all these files there.")
-} else { cat("Found C:\\home\\work") }
-Sys.setenv(HOME="C:\\home")  # needed for sourcing
+if (Sys.info()['sysname'] != "Linux") {
+    if (!("home" %in% list.files("C:\\")) | !("work" %in% list.files("C:\\home"))) {
+        cat("Need to create 'C:\\home\\work' directory and move all these files there.")
+    } else { cat("Found C:\\home\\work") }
+    Sys.setenv(HOME="C:\\home")  # needed for sourcing
+}
 
 cat("\nRunning height and bole volume calculations...\n")
 source("./boles.R")
